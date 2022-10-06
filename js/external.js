c3f238e6d3e97d38aa2fb4aa42ae16c9d5491549
Cody <cody@codeup.com>
2022-10-06 15:08:22 -0500

    pushing js with html lecture notes

---
+++ js/external.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// console.log('Hello from JavaScript!');
+//
+// alert("Happy Thorsday Xerxes");
+//
+// // the following line will show the OK/CANCEL confirm dialog
+// var confirmed = confirm('Are you sure you want to do XYZ?');
+//
+// console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+
+
+console.log('The user entered: ' + userInput);
