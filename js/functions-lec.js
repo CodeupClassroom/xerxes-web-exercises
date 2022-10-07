72b14569e1b75c191f13f7523c159ba75c76b1aa
David Stephens <stephens.a.david@gmail.com>
2022-10-07 09:27:26 -0500

    reformatted lecture code

--- js/functions-lec.js
+++ js/functions-lec.js
@@ -1,41 +1,44 @@
 "use strict";
 
 (function () {
-// alert("Hello");
-// let message = "How are you?";
-// alert(message);
-//
-// function greeting(name) {
-//     return "Howdy there, " + name + ".";
-// }
-//
-// let name = prompt("What is you name?");
-// console.log(greeting("Cody"));
-// console.log(greeting(name));
-// console.log(greeting());
+    function greeting(name) {
+        return "Howdy there, " + name + ".";
+    }
 
     function sum(a, b) {
         let total = a + b;
-        // console.log(total);
+        console.log(total);
         return total;
     }
-
-    console.log(sum(4, 3));
-// console.log(total); <- this doesn't work :(
-
-    let jeff = "Jeff";
 
     function hiJeff() {
         return "Hi " + jeff;
         console.log("You'll never see this console log. >:)");
     }
 
-    console.log(hiJeff());
-
     function getNameAndPrint() {
         let userName = prompt("Hey, what is your name?");
         alert("Nice to meet you, " + userName);
     }
+
+    alert("Hello");
+    let message = "How are you?";
+    alert(message);
+    console.log(greeting("Bill"));
+
+
+    let name = prompt("What is you name?");
+    console.log(greeting("Cody"));
+    console.log(greeting(name));
+    console.log(greeting());
+
+
+    console.log(sum(4, 3));
+    // console.log(total); // <- this doesn't work :(
+
+    let jeff = "Jeff";
+
+    console.log(hiJeff());
 
     getNameAndPrint();
 })();
