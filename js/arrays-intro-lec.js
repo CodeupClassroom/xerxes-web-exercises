b4ca1d80536c4242167ca64f28e8e44ad5235941
David Stephens <stephens.a.david@gmail.com>
2022-10-17 11:39:46 -0500

    added notes to arrays intro lec

--- js/arrays-intro-lec.js
+++ js/arrays-intro-lec.js
@@ -1,17 +1,20 @@
 (function() {
+    // Instantiating an empty array
     let testArray = [];
 
+    // Instantiating arrays with predefined elements
     let instructors = ["David", "Cody", "Codey The Duck"];
 
     const falsyValues = [false, null, "", 0, undefined, NaN];
 
+    // Access the number of elements within an array
     console.log(instructors.length);
     console.log(falsyValues.length);
     console.log([].length);
     console.log([1,1,1,1,1,1,1,1,9,9,9,9,0,0,0,10000000,11,22,23,24,34,45].length);
 
     console.log(instructors[0]);
-    instructors[0] = "'Lord Megatron' David";
+    instructors[0] = "'Lord Megatron' David"; // reassignment of element at a specific index
     instructors[1] = "'Emperor Starscream' Cody";
     console.log(instructors[0]); // first element of an array
     console.log(falsyValues[falsyValues.length - 1]); // Get the last element from an array ez
