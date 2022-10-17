475d1ee49baf01a163ea9a5a899dd65a04bbb0b3
David Stephens <stephens.a.david@gmail.com>
2022-10-17 11:17:36 -0500

    completed lecture, will add notes

---
+++ js/arrays-intro-lec.js
@@ -0,0 +1,43 @@
+(function() {
+    let testArray = [];
+
+    let instructors = ["David", "Cody", "Codey The Duck"];
+
+    const falsyValues = [false, null, "", 0, undefined, NaN];
+
+    console.log(instructors.length);
+    console.log(falsyValues.length);
+    console.log([].length);
+    console.log([1,1,1,1,1,1,1,1,9,9,9,9,0,0,0,10000000,11,22,23,24,34,45].length);
+
+    console.log(instructors[0]);
+    instructors[0] = "'Lord Megatron' David";
+    instructors[1] = "'Emperor Starscream' Cody";
+    console.log(instructors[0]); // first element of an array
+    console.log(falsyValues[falsyValues.length - 1]); // Get the last element from an array ez
+
+    let randomIndex = Math.floor(Math.random() * instructors.length);
+    alert("One of your instructors is named: " + instructors[randomIndex]);
+
+    console.log(instructors);
+
+    for(let i = instructors.length - 1; i > -1; i--) {
+        console.log("One of your instructors is named: " + instructors[i]);
+    }
+
+    instructors.forEach(function (instructor){
+        console.log("One of your instructors is named: " + instructor);
+    });
+
+    function logInfo(element) {
+        console.log(element);
+    }
+
+    falsyValues.forEach(logInfo);
+    instructors.forEach(logInfo);
+
+    for(let instructor of instructors) {
+        console.log("One of your instructors is named: " + instructor);
+    }
+
+})();
